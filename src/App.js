3a35b2e5546a1eadc178aa6e0721f37716cdaac8
Codingnaveen46 <naveenreddy4411@gmail.com>
2024-05-20 00:39:19 +0530

    firstcommit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import TodoLIst from './TodoList';
+import './App.css';
+
+
+function App(){
+  return(
+    <div className='App'>
+      <TodoLIst/>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/TodoList.js
@@ -0,0 +1,41 @@
+// src/TodoList.js
+import React, { useState } from 'react';
+
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const addTodo = () => {
+    if (newTodo.trim() !== '') {
+      setTodos([...todos, newTodo]);
+      setNewTodo('');
+    }
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = todos.filter((_, i) => i !== index);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1> ReactJs Todo List</h1>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+        placeholder="Add a new todo"
+      />
+      <button onClick={addTodo}>Add</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo} <button onClick={() => removeTodo(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
